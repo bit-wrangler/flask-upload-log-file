d5ad33bcd56d066d74a8419bc4dade89d60ded68
Aleksandr Spiridonov <sasha.aos@gmail.com>
2018-07-10 23:49:20 -0400

    Added ORM to logged value model

--- src/api/models/loggedValue.py
+++ src/api/models/loggedValue.py
@@ -1,4 +1,15 @@
-class LoggedValue:
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, DateTime, Integer, Float
+
+Base = declarative_base()
+
+class LoggedValue(Base):
+    __tablename__ = 'logged_values'
+
+    tagId = Column('tag_id',Integer, primary_key=True, ForeignKey('tags.id'), nullable=False)
+    datetime = Column('datetime',DateTime, primary_key=True, nullable=False)
+    value = Column('value',Float)
+
     def __init__(self, tagId, datetime, value):
         self.tagId = tagId
         self.datetime = datetime
