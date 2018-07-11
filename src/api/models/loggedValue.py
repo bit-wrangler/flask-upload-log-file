e4bd3d375496343279c52bb81c891d9c8763511d
Aleksandr Spiridonov <sasha.aos@gmail.com>
2018-07-10 22:20:44 -0400

    Added logged value model

---
+++ src/api/models/loggedValue.py
@@ -0,0 +1,5 @@
+class LoggedValue:
+    def __init__(self, tagId, datetime, value):
+        self.tagId = tagId
+        self.datetime = datetime
+        self.value = value
--- src/api/server.py
+++ src/api/server.py
@@ -1,7 +1,5 @@
 from flask import Flask
 from flask_restful import Api
-import werkzeug
-
 from resources.logUpload import LogUpload
 
 
